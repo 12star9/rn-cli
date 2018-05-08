e21654d53fad6eb13f739c0827a043df3ad84377
12star9 <1500021502@qq.com>
2018-05-08 22:32:30 +0900

    printName.py

---
+++ Lecture 1/4. Functions/printName.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+def printName(firstName, lastName, reverse):
+    if reverse:
+        print(lastName + ', ' + firstName)
+    else:
+        print(firstName, lastName)

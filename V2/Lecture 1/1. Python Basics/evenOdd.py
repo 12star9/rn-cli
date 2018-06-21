1f55cea637e3bf1d405802908d2bfcc8fbb9c6a2
12star9 <1500021502@qq.com>
2018-06-21 18:36:34 +0900

    evenOdd.py

---
+++ V2/Lecture 1/1. Python Basics/evenOdd.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+x = int(input('Enter an integer: '))
+if x%2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')

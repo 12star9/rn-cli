862db98cb429e572f5ab570b0b12d016d03f013d
12star9 <1500021502@qq.com>
2018-06-28 22:43:15 +0900

    printExample.py

---
+++ V2/Lecture 1/3. Iteration, Loops, Flow Control/printExample.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+
+x = 1
+print(x)
+x_str = str(x)
+print("my fav num is", x, ".", "x =", x)
+print("my fav num is " + x_str + ". " + "x = " + x_str)

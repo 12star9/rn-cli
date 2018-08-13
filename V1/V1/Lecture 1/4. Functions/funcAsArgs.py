227366d2669a8fd87734ea9a342a4d3ec5786cf8
12star9 <1500021502@qq.com>
2018-08-14 01:39:25 +0900

    funcAsArgs.py

---
+++ V1/V1/Lecture 1/4. Functions/funcAsArgs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+def func_a():
+    print('inside func_a')
+def func_b(y):
+    print('inside func_b')
+    return y
+def func_c(z):
+    print('inside func_c')
+    return z()
+print(func_a())
+print(5 + func_b(2))
+print(func_c(func_a))

0418ae81fbc8804efea64596beee52070bb68e79
12star9 <1500021502@qq.com>
2019-01-21 09:24:51 +0900

    universities.py

---
+++ v5/V1/Lecture 1/2. Common Data Structures/universities.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+US = ['MIT', 'Harvard', 'Yale']
+UK = ['Cambridge', 'Oxford']
+
+Unis = [US, UK]
+Unisnew = [['MIT', 'Harvard', 'Yale'], ['US', 'UK']]
+
+# US.append('Princeton')

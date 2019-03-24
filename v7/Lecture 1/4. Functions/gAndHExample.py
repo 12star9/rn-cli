22a8a140b729c3b5baf3749d37ce6f15f35322cf
12star9 <1500021502@qq.com>
2019-03-24 11:09:11 +0900

    gAndHExample.py

---
+++ v7/Lecture 1/4. Functions/gAndHExample.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 2018
+
+"""
+
+def g(x):
+    def h():
+        x = 'abc'
+    x = x + 1
+    print('in g(x): x =', x)
+    h()
+    return x
+
+x = 3
+z = g(x)

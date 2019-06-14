a96da1ef3a7e90eabe485206f8f5d8c026b96203
12star9 <1500021502@qq.com>
2019-06-14 16:07:12 +0800

    添加GoogleMaps

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,9 +22,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+          <MapView
+            
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+          
+          />
       </View>
     );
   }
@@ -35,6 +39,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  map:{
+    width:200,
+    height:200,
   },
   welcome: {
     fontSize: 20,
